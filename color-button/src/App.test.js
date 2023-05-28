a38950b84b171e5e17097342d17404bc2d1a8e42
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2023-05-28 19:57:34 +0530

    log-roles for debugging tests

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,14 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { logRoles } from "@testing-library/dom";
 
 test("button has correct initial color", () => {
-  render(<App />);
+  const { container } = render(<App />);
+  logRoles(container);
 
   // find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole("button", {
     name: "Change to blue",
   });
-
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 });
