9c0b422f545cd9413df35ae8eb8fb149cca36de2
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2023-05-28 21:22:31 +0530

    Test initial condition of button and checkbox

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -3,14 +3,17 @@ import { useState } from "react";
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
-    <div className="App">
+    <div>
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
       >
         Change to {newButtonColor}
       </button>
+      <br />
+      <input type="checkbox" id="disable-button-checkbox" />
     </div>
   );
 }
