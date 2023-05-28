13710534dabb1c339280ff119eb76bc27cc77a02
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2023-05-28 22:32:00 +0530

    unit testing function

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -85,3 +85,15 @@ test("Clicked disabled button has gray background and reverts to blue", () => {
   fireEvent.click(checkbox);
   expect(colorButton).toHaveStyle("background-color: blue");
 });
+
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    expect(replaceCamelWithSpaces("Red")).toBe("Red");
+  });
+  test("Works for one inner capital letter", () => {
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+  test("Works for multiple inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
+  });
+});
